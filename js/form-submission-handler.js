78164fc219e1f82215ce009ca0b2c41af5d6112a
Perseverance <pmbewe@csir.co.za>
2018-07-23 15:20:23 +0200

    added google analytics and page tracking

--- js/form-submission-handler.js
+++ js/form-submission-handler.js
@@ -28,28 +28,26 @@
         }
       }).filter(function(item, pos, self) {
         return self.indexOf(item) == pos && item;
-      });MP_ratings
-  
+      });
+      
       var formData = {};
       fields.forEach(function(name){
         var element = elements[name];
-        
         // singular form elements just have one value
         formData[name] = element.value;
-  
         // when our element has multiple items, get their values
         if (element.length) {
           var data = [];
           for (var i = 0; i < element.length; i++) {
             var item = element.item(i);
             if (item.checked || item.selected) {
-              data.push(item.value);
+                data.push(item.value);
             }
           }
           formData[name] = data.join(', ');
         }
       });
-  
+      
       // add form-specific values into the data
       formData.formDataNameOrder = JSON.stringify(fields);
       formData.formGoogleSheetName = form.dataset.sheet || "responses"; // default sheet name
@@ -62,13 +60,7 @@
     function handleFormSubmit(event) {  // handles form submit without any jquery
       event.preventDefault();           // we are submitting via xhr below
       var data = getFormData();         // get the values submitted in the form
-  
-      /* OPTION: Remove this comment to enable SPAM prevention, see README.md
-      if (validateHuman(data.honeypot)) {  //if form is filled, form will not be submitted
-        return false;
-      }
-      */
-     MP_ratings
+
       if( data.email && !validEmail(data.email) ) {   // if email is not valid show error
         var invalidEmail = document.getElementById("email-invalid");
         if (invalidEmail) {
